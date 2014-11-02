0c9e90e7470200db0128e93d449037cda9103065
osveliz <osveliz@users.noreply.github.com>
2014-11-02 16:41:49 -0700

    Create GameMaster.java
    Added an executable class.

---
+++ SecurityGame/src/securitygame/GameMaster.java
@@ -0,0 +1,18 @@
+package securitygame;
+
+public class GameMaster
+{
+    public static void main(String[] args)
+    {
+        generateGraphs(10);
+    }
+
+    public static void generateGraphs(int numGraphs)
+    {
+        for(int i = 0; i < numGraphs; i++)
+        {
+            Network n = new Network(i);
+            n.printNetwork();
+        }
+    }
+}
