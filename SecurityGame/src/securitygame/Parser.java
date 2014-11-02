087bba66b15c4288c899ecbfd12e05d0f40ed550
osveliz <osveliz@users.noreply.github.com>
2014-11-02 16:24:06 -0700

    Update Parser.java
    Allow for multiple graph generation

--- SecurityGame/src/securitygame/Parser.java
+++ SecurityGame/src/securitygame/Parser.java
@@ -4,95 +4,75 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.Iterator;
-import java.util.List;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
 
-public class Parser {
-	
+public class Parser
+{
 	public static Network parseGraph(String filename)
 	{
-		File csvtrainData = new File(filename);
-		
+		File csvTrainData = new File(filename);
 		try 
 		{
-			CSVParser parser = CSVParser.parse(csvtrainData, StandardCharsets.US_ASCII, CSVFormat.DEFAULT);
-			CSVParser parserecords= CSVParser.parse(csvtrainData, StandardCharsets.US_ASCII, CSVFormat.DEFAULT);
-			
-			int neighborscounter = 0;
-			int valuecounter = 0;
-			int numberofnodes = parserecords.getRecords().size()/2;
-			Network network = new Network(filename+"1",numberofnodes);
+			CSVParser parser = CSVParser.parse(csvTrainData, StandardCharsets.US_ASCII, CSVFormat.DEFAULT);
+			CSVParser parseRecords= CSVParser.parse(csvTrainData, StandardCharsets.US_ASCII, CSVFormat.DEFAULT);
+			int neighborsCounter = 0;
+			int valueCounter = 0;
+			int numNodes = parseRecords.getRecords().size()/2;
+			//Network network = new Network(filename+"1",numNodes);
+            Network network = new Network(0,numNodes);
 			boolean flag = false;
-			
 			for (CSVRecord csvRecord : parser) 
 			{
 				Iterator<String> itr = csvRecord.iterator();
-				if((neighborscounter<numberofnodes) && flag==false)
+				if((neighborsCounter<numNodes) && flag==false)
 				{
-					Node node = network.getNode(neighborscounter);
+					Node node = network.getNode(neighborsCounter);
 					while(itr.hasNext())
 					{
 						int x = Integer.parseInt(itr.next());
 						Node neighbor = network.getNode(x);
 						node.addNeighbor(neighbor);
-						
-						
 					}
-					
-					if(neighborscounter==numberofnodes-1)
+					if(neighborsCounter==numNodes-1)
 					{
-						flag= true;
-						neighborscounter=0;
+						flag = true;
+						neighborsCounter=0;
 					}
 					else
-					{
-						neighborscounter++;
-					}
-					
+						neighborsCounter++;
 				}
-				else if(flag==true && (neighborscounter<numberofnodes))
+				else if(flag && (neighborsCounter<numNodes))
 				{
 					int counter = 0;
-					Node node = network.getNode(neighborscounter);
+					Node node = network.getNode(neighborsCounter);
 					while(itr.hasNext())
 					{
 						if(counter==2)
 						{
 							String x = itr.next();
-							node.setIspublic(Boolean.parseBoolean(x));
-							
+							node.setPublic(Boolean.parseBoolean(x));
 						}
 						else if(counter<2)
 						{
-							
 							int x  =  Integer.parseInt(itr.next());
 							node.setPv(x);
 							int y = Integer.parseInt(itr.next());
 							node.setSv(y);
-							
 						}
 						counter+=2;
 					}
-					neighborscounter++;
+					neighborsCounter++;
 				}
 			}
-			
 			return network;
 		}
 		catch (IOException e) 
 		{
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
 		return null;
-		
 	}
-	
-	
-	
-
 }
