86056e89b9bc40ccabf4c1ac7c0a0fec54df298a
Oscar Veliz <osveliz@miners.utep.edu>
2014-11-06 01:09:26 -0700

    Simplified and Bug Fixed DefenderHelper
    Also included JavaDoc for these two classes.

--- SecurityGame/src/securitygame/Defender.java
+++ SecurityGame/src/securitygame/Defender.java
@@ -1,42 +1,80 @@
 package securitygame;
 
-import com.sun.xml.internal.fastinfoset.tools.PrintTable;
-import org.omg.Dynamic.Parameter;
-
-import java.io.PrintWriter;
-import java.util.Arrays;
 import java.util.Random;
 
+/**
+ * Defender agent. The actions for the defender in this game include strengthening nodes, adding firewalls, and adding honeypots.
+ * All logic/equations/formulas/etc for how your defender decides to select actions should be included in run()
+ */
 public class Defender
 {
-    private String name;
     private Network net;
     private DefenderHelper dh;
-    public Defender(String newName, String graphFile)
+
+    /**
+     * Constructor.
+     * Parses Network stored in graphFile.
+     * Performs Defender logic to select actions.
+     * Outputs [agentName]-[graphFile].defense with selected actions
+     * @param agentName Defender agent's name i.e. "Miners"
+     * @param graphFile String containing number of network i.e. "1914"
+     */
+    public Defender(String agentName, String graphFile)
     {
-        name = newName;
         net = Parser.parseGraph(graphFile+".graph");
-        dh = new DefenderHelper(net,graphFile,name);
+        dh = new DefenderHelper(net,graphFile, agentName);
         run();
         dh.close();
+        System.out.println("completed "+ agentName +"-"+graphFile+".graph");
     }
-    public void invalid()
-    {
-        dh.invalid();
-    }
+
+    /**
+     * Defender selects to strengthen a node by 1 sv.
+     * Only works on non-public nodes that are not already at maximum strength (20).
+     *
+     * Recommend using isValidStrengthen() in DefenderHelper before committing to this action
+     *
+     * @param id Node's ID number
+     */
     public void strengthen(int id)
     {
         dh.strengthen(id);
     }
 
+    /**
+     * Defender selects to remove the edge between two nodes (firewalling them).
+     * Only works when doing so would not create island nodes and if there actually is an edge between them.
+     *
+     * Recommend using isValidFirewall() in DefenderHelper before committing to this action.
+     *
+     * @param id1 ID number of first node
+     * @param id2 ID number of second node
+     */
     public void firewall(int id1, int id2)
     {
         dh.firewall(id1,id2);
     }
+
+    /**
+     * Defender selects to add a honeypot and gets to pick the Security Value, Point Value, and neighboring connected nodes.
+     * Attacker will not get the Point Value for this node after a successful attack.
+     * Security Value cannot be 0 (reserved for public nodes) and not greater than 20 (maximum). Points cannot be more than 20.
+     * Cannot connect to nodes not in graph.
+     *
+     * Recommend using isValidHoneypot() in DefenderHelper before committing to this action.
+     *
+     * @param sv Desired Security Value should be > 0 and <= 20
+     * @param pv Desired Point Value should be >= 0 and <= 20
+     * @param newNeighbors array of Node IDs to connect this honeypot to.
+     */
     public void honeypot(int sv, int pv, int[] newNeighbors)
     {
         dh.honeypot(sv,pv,newNeighbors);
     }
+
+    /**
+     * Add your decision logic here.
+     */
     public void run()
     {
         Random r = new Random();
--- SecurityGame/src/securitygame/DefenderHelper.java
+++ SecurityGame/src/securitygame/DefenderHelper.java
@@ -5,12 +5,23 @@ import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
 
 import java.io.File;
-import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
+
+/**
+ * Axillary class for creating and parsing defence files.
+ * Defender will use the three parameter constructor and combination of strengthen(), firewall, and honeypot()
+ * to generate defence file. Have Defender remember to call close() when finished for safety.
+ * Game Master will use the two parameter constructor for parsing original network and defence file to generate new network.
+ *
+ * Actions deemed invalid will be charged the Parameters.INVALID_RATE value.
+ *
+ * @author      Oscar Veliz
+ * @version     2014/11/01
+ */
 
 public class DefenderHelper
 {
@@ -18,131 +29,156 @@ public class DefenderHelper
     private String name;
     private PrintWriter pw;
     private int budget;
-    public DefenderHelper(Network n, String file, String s)
+
+    /**
+     * Constructor used by Defender to initialize defence file and keep track of network changes.
+     * @param network Graph being secured given a budget
+     * @param graphFile Contains original name of graph i.e. "1" for 1.graph
+     * @param defenderName Name of defender will be prepended to defence file i.e. "tower" for tower-1.defence
+     */
+    public DefenderHelper(Network network, String graphFile, String defenderName)
     {
         budget = Parameters.DEFENDER_BUDGET;
-        net = n;
-        name = s;
+        net = network;
+        name = defenderName;
         try
         {
-            pw = new PrintWriter(name+"-"+file + ".defence", "UTF-8");
+            pw = new PrintWriter(name+"-"+graphFile + ".defence", "UTF-8");
         }
         catch (Exception e)
         {
             e.printStackTrace();
         }
     }
+
+    /**
+     * Constructor used by GameMaster to create new secured graph based on Defender's defence actions.
+     * @param defenderName Name of defender prepended to defence file i.e. "tower" for tower-1.defence
+     * @param graphFile Contains original name of graph i.e. "1" for 1.graph
+     */
     public DefenderHelper(String defenderName, String graphFile){
+        name = defenderName;
         budget = Parameters.DEFENDER_BUDGET;
         net = Parser.parseGraph(graphFile+".graph");
-        File csvTrainData = new File(defenderName+"-"+graphFile+".defence");
+        File csv = new File(defenderName+"-"+graphFile+".defence");
 		try
 		{
-			CSVParser parser = CSVParser.parse(csvTrainData, StandardCharsets.US_ASCII, CSVFormat.DEFAULT);
-			//CSVParser parseRecords= CSVParser.parse(csvTrainData, StandardCharsets.US_ASCII, CSVFormat.DEFAULT);
+			CSVParser parser = CSVParser.parse(csv, StandardCharsets.US_ASCII, CSVFormat.DEFAULT);
 			for (CSVRecord csvRecord : parser)
 			{
 				Iterator<String> itr = csvRecord.iterator();
                 int mode = Integer.parseInt(itr.next());
                 switch (mode){
                     case 0://strengthen
-                        if(budget > Parameters.STRENGTHEN_RATE){
-                            int id = Integer.parseInt(itr.next());
+                        int id = Integer.parseInt(itr.next());
+                        if(isValidStrengthen(id))
+                        {
+                            budget -= Parameters.STRENGTHEN_RATE;
                             Node n = net.getNode(id);
-                            if(n != null){
-                                budget -= Parameters.STRENGTHEN_RATE;
-                                n.setSv(n.getSv()+1);
-                            }
-                            else
-                                budget -= Parameters.INVALID_RATE;
+                            n.setSv(n.getSv()+1);
                         }
+                        else
+                            budget -= Parameters.INVALID_RATE;
                         break;
                     case 1://firewall
-                        if(budget < Parameters.FIREWALL_RATE)
-                            budget -= Parameters.INVALID_RATE;
-                        else
-                        {
-                            int id1 = Integer.parseInt(itr.next());
-                            int id2 = Integer.parseInt(itr.next());
+                        int id1 = Integer.parseInt(itr.next());
+                        int id2 = Integer.parseInt(itr.next());
+                        if(isValidFirewall(id1,id2)){
                             Node n1 = net.getNode(id1);
                             Node n2 = net.getNode(id2);
-                            if(n1 == null || n2 == null)
-                                budget -= Parameters.INVALID_RATE;
-                            else if(n1.neighbor.size()==1 || n2.neighbor.size()==1)
-                                budget -= Parameters.INVALID_RATE;
-                            else if(n1.neighbor.contains(n2))
-                            {
-                                n1.neighbor.remove(n2);
-                                n2.neighbor.remove(n1);
-                                budget -= Parameters.FIREWALL_RATE;
-                            }
+                            n1.neighbor.remove(n2);
+                            n2.neighbor.remove(n1);
+                            budget -= Parameters.FIREWALL_RATE;
                         }
+                        else
+                            budget -= Parameters.INVALID_RATE;
+
                         break;
                     case 2://honeypot
-                        if(budget < Parameters.HONEYPOT_RATE)
-                            budget -= Parameters.INVALID_RATE;
-                        else
+                        int sv = Integer.parseInt(itr.next());
+                        int pv = Integer.parseInt(itr.next());
+                        ArrayList<Integer> newNeighbors = new ArrayList<Integer>();
+                        while (itr.hasNext())
+                            newNeighbors.add(Integer.parseInt(itr.next()));
+                        int[] n = new int[newNeighbors.size()];
+                        for(int i = 0; i < n.length; i++)
+                            n[i] = newNeighbors.get(i);
+                        if(isValidHoneypot(sv, pv, n))
                         {
-                            int sv = Integer.parseInt(itr.next());
-                            int pv = Integer.parseInt(itr.next());
-                            ArrayList<Integer> newNeighbors = new ArrayList<Integer>();
-                            while (itr.hasNext())
-                                newNeighbors.add(Integer.parseInt(itr.next()));
-                            int[] n = new int[newNeighbors.size()];
-                            for(int i = 0; i < n.length; i++)
-                                n[i] = newNeighbors.get(i);
                             net.addHoneypot(sv, pv, n);
                             budget -= Parameters.HONEYPOT_RATE;
                         }
+                        else
+                            budget -= Parameters.INVALID_RATE;
                      break;
-                     default://invald
+                     default://some other case not defined
                         budget -= Parameters.INVALID_RATE;
                      break;
                 }
             }
+            parser.close();
         }catch (Exception e){
             e.printStackTrace();
         }
-        net.setName(defenderName+"-"+graphFile);
+<<<<<<< HEAD
+        net.setName(name+"-"+graphFile);
+=======
+        net.setName(Integer.parseInt(defenderName+"-"+graphFile));
+>>>>>>> origin/master
         net.printNetwork();
     }
 
+    /**
+     * Defender should call this method when done adding actions.
+     */
     public void close()
     {
         pw.close();
     }
+
+    /**
+     * Action has been deemed invalid
+     */
     public void invalid()
     {
         budget -= Parameters.INVALID_RATE;
         pw.write("-1");
         pw.println();
     }
+
+    /**
+     * Adds 1 to a node security value if the node is not public or not already at maximum security.
+     * @param id The id of the node being strengthened
+     */
     public void strengthen(int id)
     {
-        if(budget < Parameters.STRENGTHEN_RATE)
-            invalid();
-        else if(net.getNode(id)!=null && net.getNode(id).getPv()!=0)
+        if(isValidStrengthen(id))
         {
+            Node n = net.getNode(id);
             budget -= Parameters.STRENGTHEN_RATE;
-            net.getNode(id).setSv(net.getNode(id).getSv());
+            n.setSv(n.getSv());
             pw.write("0,"+id);
             pw.println();
         }
         else
             invalid();
     }
+
+    /**
+     * Removes the edge between two nodes. Will not remove if doing so will isolate a node. Will not remove if there is no
+     * edge to remove.
+     *
+     * @param id1 First node's id
+     * @param id2 Second node's id
+     */
     public void firewall(int id1, int id2)
     {
-        if(budget < Parameters.FIREWALL_RATE)
-            invalid();
-        else if(net.getNode(id1) == null || net.getNode(id2) == null)
-            invalid();
-        else if(net.getNode(id1).neighbor.size()==1 || net.getNode(id2).neighbor.size()==1)
-            invalid();
-        else if(net.getNode(id1).neighbor.contains(net.getNode(id2)))
+        if(isValidFirewall(id1, id2))
         {
-            net.getNode(id1).neighbor.remove(net.getNode(id2));
-            net.getNode(id2).neighbor.remove(net.getNode(id1));
+            Node n1 = net.getNode(id1);
+            Node n2 = net.getNode(id2);
+            n1.neighbor.remove(n2);
+            n2.neighbor.remove(n1);
             budget -= Parameters.FIREWALL_RATE;
             pw.write("1,"+id1+","+id2);
             pw.println();
@@ -151,18 +187,17 @@ public class DefenderHelper
             invalid();
 
     }
+
+    /**
+     * Adds a honeypot node to the graph if possible. Otherwise charges an invalid.
+     * @param sv Security Value for the honeypot
+     * @param pv Point Value for the honeypot
+     * @param newNeighbors Array of Node ID's specifying which nodes to connect the honeypot to
+     */
     public void honeypot(int sv, int pv, int[] newNeighbors)
     {
-        if(budget < Parameters.HONEYPOT_RATE)
-            invalid();
-        else{
-            Arrays.sort(newNeighbors);
-            for(int i = 0; i < newNeighbors.length-1;i++)
-                if(newNeighbors[i]==newNeighbors[i+1] || net.getNode(newNeighbors[i])==null)
-                {
-                    invalid();
-                    return;
-                }
+        if(isValidHoneypot(sv, pv, newNeighbors))
+        {
             net.addHoneypot(sv, pv, newNeighbors);
             budget -= Parameters.HONEYPOT_RATE;
             String s = "2,"+sv+","+pv+",";
@@ -172,9 +207,62 @@ public class DefenderHelper
             pw.write(s);
             pw.println();
         }
+        else
+            invalid();
     }
+
+    /**
+     * Returns current budget.
+     * @return current budget
+     */
     public int getBudget()
     {
         return budget;
+    }
+
+    public boolean isValidStrengthen(int id)
+    {
+        if(budget < Parameters.STRENGTHEN_RATE)
+            return false;
+        else
+        {
+            Node n = net.getNode(id);
+            if(n != null && n.getSv()!=20 && n.getSv()!=0)
+                return true;
+            else //can't strengthen public node or maxed out node
+                return false;
+        }
+    }
+
+    public boolean isValidFirewall(int id1, int id2)
+    {
+        if(budget < Parameters.FIREWALL_RATE)
+            return false;
+        else
+        {
+            Node n1 = net.getNode(id1);
+            Node n2 = net.getNode(id2);
+            if(n1 == null || n2 == null)
+                return false;
+            else if(n1.neighbor.size()==1 || n2.neighbor.size()==1)//can't make islands
+                return false;
+            else return n1.neighbor.contains(n2);
+        }
+    }
+
+    public boolean isValidHoneypot(int sv, int pv, int[] newNeighbors){
+        if(budget < Parameters.HONEYPOT_RATE)
+            return false;
+        else{
+            if(sv <= 0 || sv > 20 || pv < 0 || pv > 20)//validate sv and pv
+                return false;
+            //check if there are two of the same neighbor (indicator that something is wrong
+            //and that all of the nodes being connected to exist
+            Arrays.sort(newNeighbors);
+            for(int i = 0; i < newNeighbors.length-1;i++)
+                if(newNeighbors[i]==newNeighbors[i+1] || net.getNode(newNeighbors[i])==null)
+                    return false;
+            return true;
+        }
     }
 }
